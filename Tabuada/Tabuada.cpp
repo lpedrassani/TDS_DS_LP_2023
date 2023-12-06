b8d8684dc5b9865970d6f0f1f566eef246d856e9
Leonardo Pedrassani <leopedrassani201@gmail.com>
2023-12-05 21:58:49 -0300

    Resolução do Exercício Tabuada

--- Tabuada/Tabuada.cpp
+++ Tabuada/Tabuada.cpp
@@ -3,22 +3,18 @@
 int main()
 {
     int tabuada[9];
-    int i, j, result=0, soma=0, somaTotal=0;
-
+    int i, j, somaTotal=0, resultado[11];
 
     for(i=1;i<=10;i++)
     {
-        tabuada[i]=0;
+        resultado[i]=0;
         for(j=1;j<=10;j++)
         {
-            result=i*j;
-            printf("%d x %d = %d\n", i, j, result);
-            soma = soma+result;
+            resultado[i]=resultado[i]+(i*j);
+            printf("%d x %d = %d\n", i, j, resultado[i]);
         }
-        tabuada[i]= tabuada[i]+soma;
-        printf("A soma da tabuada %d eh: %d\n", i, tabuada[i]);
-        soma=0;
-        somaTotal=somaTotal+tabuada[i];
+        printf("A soma da tabuada %d eh: %d\n", i, resultado[i]);
+        somaTotal=somaTotal+resultado[i];
     }
     printf("\nA soma total das tabuadas eh: %d\n", somaTotal);
     return 0;
